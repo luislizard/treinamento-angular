c6feac9a7599af07bd2c2db0dae8f3a6f6355825
luislizard <luislizard@ymail.com>
2021-12-02 12:03:09 +0000

    aula-7

--- src/app/post-view/comments/comments-form/comments-form.component.ts
+++ src/app/post-view/comments/comments-form/comments-form.component.ts
@@ -1,27 +1,39 @@
+import { Comments } from './../interfaces/comments.interface';
+import { CommentsDataService } from './../services/comments-data.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-comments-form',
   templateUrl: './comments-form.component.html',
-  styleUrls: ['./comments-form.component.css']
+  styleUrls: ['./comments-form.component.css'],
 })
 export class CommentsFormComponent implements OnInit {
-
   commentForm: FormGroup;
 
-  constructor() {
+  constructor(private commentsDataService: CommentsDataService) {
     this.commentForm = new FormGroup({
-      commentTxt: new FormControl('', [Validators.required, Validators.minLength(10)])
-    })
-   }
+      commentTxt: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
+  }
 
   ngOnInit(): void {
-    console.log(this.commentForm.controls['commentTxt'].errors)
+    console.log(this.commentForm.controls['commentTxt'].errors);
   }
 
-  sendForm(){
-    console.log(this.commentForm)
-  }
+  sendForm() {
+    const comentario: Comments = {
+      createdAt: '',
+      userId: 123,
+      postId: 123,
+      comment: this.commentForm.value.commentTxt,
+    };
 
+    this.commentsDataService
+      .postComments(comentario)
+      .subscribe((res) => console.log(res));
+  }
 }
